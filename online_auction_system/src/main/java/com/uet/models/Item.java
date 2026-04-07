d8607cf77c253c9451165c17b5b8cf3380d4fff0
Lưu Đình Nguyện <nguyenld27@gmail.com>
2026-04-07 07:43:58 +0700

    thêm abstract Entity vào OPP

--- online_auction_system/src/main/java/com/uet/models/Item.java
+++ online_auction_system/src/main/java/com/uet/models/Item.java
@@ -1,30 +1,27 @@
 package com.uet.models;
 
-public class Item{
+public class Item extends Entity {
     static private int sid = 0;
     private String name;
-    private int ID;
     private double price;
     private double current_price;
 
     public Item() {}
     public Item(String name, double price) {
         this.name = name;
-        this.ID = ++this.sid;
+        super.id = "Item-" + ++this.sid;
         this.price = price;
     }
     public Item(Item x) {
         this(x.name, x.price);
-        this.ID = ++this.sid;
+        this.id = "Item-" + ++this.sid;
     }
     
 
     public String getName(){return this.name;}
-    public int getID(){return this.ID;}
+    public void setName(String Name){this.name = Name;}
     public double getPrice(){return this.price;}
     public double getFinal_Price(){return this.current_price;}
-
-    public void setName(String Name){this.name = Name;}
     public void setFinal_Price(double Final_Price){this.current_price = Final_Price;}
 
     @Override
